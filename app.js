eb1f77fb1c54665075415b584a052cbe0e26e77c
unknown <vanthong1214@gmail.com>
2022-11-14 04:45:31 +0700

    init

---
+++ app.js
@@ -0,0 +1,67 @@
+﻿ $(function(){
+    $(window).on('scroll',function(){
+        if($(window).scrollTop()){
+            $('#main-nav').addClass('arctica-sticky');
+        }
+        else{
+            $('#main-nav').removeClass('arctica-sticky');
+        }
+    });
+    //  Restaurant //
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+   // Menu Food//
+    $('.gallery').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        
+        }
+    });
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 600) {
+          $('.scroll-top').fadeIn(600);
+        } else {
+          $('.scroll-top').fadeOut(600);
+        }
+      });
+      $('.scroll-top').on("click", function () {
+        $("html,body").animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+
+    $('.magnific-images').magnificPopup({
+        delegate: '.popimg',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+});  
+ 
